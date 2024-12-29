1b5ec18b09d47c47cf0581f84b958cdff891c568
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-12-29 14:12:34 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Globalization;
+
+public class MyMatrix
+{
+    private int m; //строки
+    private int n; //столбцы
+    private int[,] Matrix;
+    private int d1;
+    private int d2;
+    private Random rand;
+
+
+    public MyMatrix(int M, int N, int D1, int D2) //конструктор
+    {
+        if (M <= 0 || N <= 0)
+        {
+            throw new ArgumentException("Количество строк и столбцов должны быть положительными");
+        }
+
+        if(D1 > D2)
+        {
+            throw new ArgumentException("Минимальное значение не должно быть больше максимального");
+        }
+
+        m = M;
+        n = N;
+        Matrix = new int[M, N];
+        rand = new Random();
+        d1 = D1;
+        d2 = D2;
+
+        for(int i = 0; i < M; ++i) //заполнение матрицы случайными числами из диапазона d1 - d2
+        {
+            for(int j = 0; j < N; ++j)
+            {
+                Matrix[i, j] = rand.Next(D1, D2);
+            }
+        }
+    }
+
+    public int this[int m, int n] //индексатор
+    {
+        get => Matrix[m, n]; //аксессор
+        set => Matrix[m, n] = value; //мутатор
+    }
+
+    public void Fill() //перезаполнение матрицы случайными числами
+    {
+        for(int i = 0; i < m; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                Matrix[i, j] = rand.Next();
+            }
+        }
+    }
+
+    public void ChangeSize(int NewM, int NewN) //изменение размера матрицы
+    {
+        if(NewM <= 0 || NewN <= 0)
+        {
+            throw new ArgumentException("Новое количество строк и столбцов должно быть положительным");
+        }
+
+        int[,] newMatrix = new int[NewM, NewN]; //создание новой матрицы
+
+        //копирование старой матрицы в новую
+        for(int i = 0; i < Math.Min(NewM, m); ++i)
+        {
+            for(int j = 0; j < Math.Min(NewN, n); ++j)
+            {
+                newMatrix[i, j] = Matrix[i, j];
+            }
+        }
+        Matrix = newMatrix;
+
+        //если новая матрица больше старой
+        if(NewM > m || NewN > n)
+        {
+            m = NewM;
+            n = NewN;
+            Fill();
+        }
+
+        m = NewM;
+        n = NewN;
+    }
+
+    public void ShowPartial(int startn, int endn, int startm, int endm) //вывод частичной матрицы
+    {
+        if(endn > n || endm > m)
+        {
+            throw new AbandonedMutexException("Число выводимых строк и столбцов не должно превышать m и n");
+        }
+        for(int i = startm; i < endm; ++i)
+        {
+            for(int j = startn; j < endn; ++j)
+            {
+                Console.Write(Matrix[i, j] + '\t');
+            }
+            Console.WriteLine();
+        }
+    }
+    public void Show() //печать матрицы
+    {
+        for(int i = 0; i < m; ++i)
+        {
+            for(int j = 0; j < m; ++j)
+            {
+                Console.Write(Matrix[i, j] + ' ' + '\t');
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число строк матрицы: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Введите число столбцов матрицы: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите минимальное число диапазона: ");
+        int d1 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Введите максимальное число диапазона: ");
+        int d2 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        MyMatrix matrix = new MyMatrix(m, n, d1, d2); 
+        Console.WriteLine("Матрица:");
+        matrix.Show();
+        Console.WriteLine("Введите номер минимально выводимой строки: ");
+        int startm = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите номер минимально выводимого столбца: ");
+        int startn = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите номер максимально выводимой строки: ");
+        int endm = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите номер максимально выводимого столбца: ");
+        int endn = int.Parse(Console.ReadLine());
+        Console.WriteLine("Частично выведенная матрица: ");
+        matrix.ShowPartial(startm, startn, endm, endn);
+        Console.WriteLine("Введите новое количество строк матрицы: ");
+        int newm = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите новое количество столбцов матрицы: ");
+        int newn = int.Parse(Console.ReadLine());
+        matrix.ChangeSize(newm, newn);
+        Console.WriteLine("Новая матрица: ");
+        matrix.Show();
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+
+public class MyList<T> :IEnumerable<T>
+{ 
+    private T[] items;
+    private int _count;
+    private const int _initialCapacity = 4; // Начальная емкость
+
+    // Конструктор по умолчанию
+    public MyList()
+    {
+        items = new T[_initialCapacity];
+        _count = 0;
+    }
+
+    // Индексатор для доступа к элементам
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= _count)
+                throw new IndexOutOfRangeException("Индекс находится вне диапазона.");
+            return items[index];
+        }
+        set
+        {
+            if (index < 0 || index >= _count)
+                throw new IndexOutOfRangeException("Индекс находится вне диапазона.");
+            items[index] = value;
+        }
+    }
+
+    // Свойство для получения общего количества элементов
+    public int Count => _count;
+
+    // Метод для добавления элемента
+    public void Add(T item)
+    {
+        if (_count == items.Length)
+        {
+            Resize();
+        }
+        items[_count++] = item;
+    }
+
+    // Метод для изменения размера массива
+    private void Resize()
+    {
+        int newCapacity = items.Length * 2; // Увеличиваем размер в 2 раза
+        T[] newItems = new T[newCapacity];
+        Array.Copy(items, newItems, _count);
+        items = newItems;
+    }
+
+    // Метод для инициализации коллекции
+    public void Initialize(params T[] items)
+    {
+        foreach (var item in items)
+        {
+            Add(item);
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+       return items.ToList().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Пример использования MyList
+        MyList<int> myList = new MyList<int>();
+
+        // Добавляем элементы
+        myList.Add(1);
+        myList.Add(2);
+        myList.Add(3);
+
+        // Вывод количества элементов
+        Console.WriteLine("Количество элементов: " + myList.Count);
+
+        // Получаем элемент по индексу
+        Console.WriteLine("Элемент по индексу 1: " + myList[1]);
+
+        // Инициализация коллекции
+        MyList<string> stringList = new MyList<string>();
+        stringList.Initialize("Hello", "World", "!");
+
+        Console.WriteLine("Количество строк в stringList: " + stringList.Count);
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            Console.WriteLine("Элемент по индексу " + i + ": " + stringList[i]);
+        }
+    }
+}
